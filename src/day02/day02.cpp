f4b8336c0eade63f0f9c284574cc6de1c70049bb
Xavi Lamela <xavierivan2000@hotmail.com>
2024-12-02 20:09:21 -0500

    add day02 solved

--- src/day02/day02.cpp
+++ src/day02/day02.cpp
@@ -30,13 +30,29 @@ namespace Day02 {
                 int a = std::stoi(report[i]);
                 int b = std::stoi(report[i+1]);
                 int difference = std::abs(b - a);
-                fmt::print("a:{}, b:{}, diff:{}\n",a,b, difference);
+                //fmt::print("a:{}, b:{}, diff:{}\n",a,b, difference);
                 if ( difference < 1 || difference > 3) {
                     //fmt::print("UNSAFE\n");
                     return false;
                 } 
             }
         return true;
+    }
+
+    bool isSafe(std::vector<std::string> report) {
+        if (isMonotonic(report) && isDiffInRange(report) ) return true;
+        return false;
+    }
+
+    bool isSafeWithDampener(std::vector<std::string>& report) {
+            for (int i = 0; i < report.size(); i++) {
+                std::vector<std::string> modreport = report;
+                 modreport.erase(modreport.begin() + i);
+                if(isSafe(modreport)) {
+                    return true;
+                }
+            }   
+        return false;
     }
 
     void SolvePart1() {
@@ -46,8 +62,8 @@ namespace Day02 {
             fmt::print("total reports: {}\n",data.words.size());
             int safe_count = 0;
             for (int n = 0; n < data.words.size(); n++) {
-                //checking monotonic and monotonic
-                if (isMonotonic(data.words[n]) && isDiffInRange(data.words[n]) ) {
+                //checking monotonic and diff is range
+                if (isSafe(data.words[n])) {
                     //fmt::print("report {} is Monotonic and diff is range\n", n);
                     safe_count++;
                 }
@@ -57,8 +73,23 @@ namespace Day02 {
     }
 
     void SolvePart2() {
-        //fmt::print("problema 2 parte 2\n");
-        //FileData data = ReadFromFile("samples/two_sample.txt");
+        FileData data = ReadFromFile("samples/two.txt");
+        if (!data.words.empty()) {
+            fmt::print("total reports: {}\n",data.words.size());
+            int safe_count = 0;
+            for (int n = 0; n < data.words.size(); n++) {
+                if (isSafe(data.words[n])) {
+                    safe_count++;
+                    continue;
+                }
+                //check removing level for safe
+                if (isSafeWithDampener(data.words[n])) {
+                    //fmt::print("report {} is safe with damper\n", n);
+                    safe_count++;
+                }
+            }
+            fmt::print("\n\t total SAFE reports:{} \n", safe_count);
+        }
     }
 
 }
